3cae501a0689c83f7a55a1327fd74c9560f2f7cc
Alexey Ignatik <ignatiklesha@gmail.com>
2016-07-14 23:10:36 +0300

    troubles fixed

--- BookClass/Book.cs
+++ BookClass/Book.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BookClass
 {
-    public class Book: IEquatable<Book>, IComparable<Book>
+    public class Book: IEquatable<Book>, IComparable<Book>, IComparable
     {
         private int year;
         private int pages;
@@ -75,33 +75,24 @@ namespace BookClass
 
         public override int GetHashCode() => Author[0] * Year + Title[0] * Pages + 2 * (Year + Pages);
 
-
-        public int CompareTo(Book book)
+        int IComparable.CompareTo(object b)
         {
-            if (ReferenceEquals(book, null))
+            if (ReferenceEquals(b, null))
                 return -1;
-            if (ReferenceEquals(this, book))
+            if (ReferenceEquals(this, b))
                 return 1;
-
-            if (Author.CompareTo(book.Author) != 0)
-                return Author.CompareTo(book.Author);
-
-            if (Title.CompareTo(book.Title) != 0)
-                return Title.CompareTo(book.Title);
-
-            if (Year > book.Year)
-                return 1;
-            else if (Year == book.Year)
-                return 0;
-            else if (Year < book.Year)
-                return -1;
-
-            if (Pages > book.Pages)
-                return 1;
-            else if (Pages == book.Pages)
-                return 1;
-            else return -1;
+            return CompareTo((Book)b);
         }
+
+        public int CompareTo(Book b)
+        {
+            if (ReferenceEquals(b, null))
+                return -1;
+            if (ReferenceEquals(this, b))
+                return 1;
+            return Title.CompareTo(b.Title);
+        }
+
 
         public override string ToString()
         {
@@ -109,38 +100,5 @@ namespace BookClass
             result.AppendFormat("Title: {0}, Author: {1}, Year: {2}, Pages: {3}.", Title, Author, Pages);
             return string.Format("{0}", result);
         }
-
-
-        public static Book[] Sort(Book[] books, IComparer<Book> comparer)
-        {
-            if (comparer == null)
-                throw new ArgumentNullException();
-
-            foreach (var book in books)
-            {
-                if (book == null)
-                    throw new ArgumentNullException();
-            }
-
-            for (int i = 0; i < books.Length; i++)
-            {
-                for (int j = i + 1; j < books.Length; j++)
-                {
-                    if (comparer.Compare(books[i], books[j]) > 0)
-                    {
-                        Swap(ref books[i], ref books[j]);
-                    }
-                }
-            }
-            return books;
-        }
-        private static void Swap(ref Book lhs, ref Book rhs)
-        {
-            var temp = lhs;
-            lhs = rhs;
-            rhs = temp;
-        }
-
-
     }
 }
---
+++ BookClass/BookOperations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookClass
+{
+    class BookOperations
+    {
+        public static Book[] Sort(Book[] books, IComparer<Book> comparer)
+        {
+            if (comparer == null)
+                throw new ArgumentNullException();
+
+            foreach (var book in books)
+            {
+                if (book == null)
+                    throw new ArgumentNullException();
+            }
+
+            for (int i = 0; i < books.Length; i++)
+            {
+                for (int j = i + 1; j < books.Length; j++)
+                {
+                    if (comparer.Compare(books[i], books[j]) > 0)
+                    {
+                        Swap(ref books[i], ref books[j]);
+                    }
+                }
+            }
+            return books;
+        }
+
+        private static void Swap(ref Book lhs, ref Book rhs)
+        {
+            var temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+    }
+}
