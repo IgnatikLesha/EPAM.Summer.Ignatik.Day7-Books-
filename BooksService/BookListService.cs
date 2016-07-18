7dbadafa25af5546b39ca4964f115fa74a6c78f7
Alexey Ignatik <ignatiklesha@gmail.com>
2016-07-18 19:09:50 +0300

    book service (not all)

---
+++ BooksService/BookListService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookClass;
+using NLog;
+
+namespace BooksService
+{
+    class BookListService
+    {
+
+        private List<Book> Books { get; }
+
+        public BookListService(List<Book> books)
+        {
+            Books = books;
+        }
+
+        public void AddBook(Book book)
+        {
+            if(!Books.Contains(book))
+                Books.Add(book);
+        }
+
+        public void RemoveBook(Book book)
+        {
+            if (Books.Contains(book))
+                Books.Remove(book);
+        }
+
+        public void SortBooksByTag(IComparer<Book> comparer)
+        {
+            Books.Sort(comparer);
+        }
+
+        public Book FindBookByTag()
+        {
+
+        }
+    }
+}
---
+++ BooksService/IBookListStorage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookClass;
+
+namespace BooksService
+{
+    interface IBookListStorage
+    {
+            
+        List<Book> LoadBooks();
+        void SaveBooks(IEnumerable<Book> books);
+    }
+}
+
