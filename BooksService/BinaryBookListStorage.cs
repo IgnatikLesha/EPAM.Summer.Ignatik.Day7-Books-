41e0422c524e8989f3dda9fac570fc3689e8f001
Alexey Ignatik <ignatiklesha@gmail.com>
2016-07-18 19:41:56 +0300

    +

--- BooksService/BinaryBookListStorage.cs
+++ BooksService/BinaryBookListStorage.cs
@@ -3,11 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using BookClass;
 
 namespace BooksService
 {
     class BinaryBookListStorage : IBookListStorage
     {
+        private string filePath;
+        public BinaryBookListStorage(string path)
+        {
+            filePath = path;
+        }
+
+        public List<Book> LoadBooks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveBooks(IEnumerable<Book> books)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- BooksService/BookListService.cs
+++ BooksService/BookListService.cs
@@ -20,24 +20,35 @@ namespace BooksService
 
         public void AddBook(Book book)
         {
-            if(!Books.Contains(book))
+            if (ReferenceEquals(book, null))
+                throw new ArgumentException();
+            if (!Books.Contains(book))
                 Books.Add(book);
         }
 
         public void RemoveBook(Book book)
         {
+            if (ReferenceEquals(book, null))
+                throw new ArgumentException();
             if (Books.Contains(book))
                 Books.Remove(book);
         }
 
         public void SortBooksByTag(IComparer<Book> comparer)
         {
+            if (ReferenceEquals(comparer, null))
+                throw new ArgumentException();
             Books.Sort(comparer);
         }
 
-        public Book FindBookByTag()
+        public List<Book> FindBookByTag(Predicate<Book> match)
         {
-
+            List<Book> result = new List<Book>();
+            foreach (var book in Books.FindAll(match))
+            {
+                result.Add(book);
+            }
+            return result;
         }
     }
 }
